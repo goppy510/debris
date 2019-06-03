1d6a4eea422a84cc43f46eb8d9a992262a5793b1
goppy510 <tacumin.goppy.06@vesta.ocn.ne.jp>
2019-06-03 21:04:57 +0900

    ページスクロールのjsã追加

--- footer.php
+++ footer.php
@@ -1,6 +1,29 @@
     </body>
     <footer class="page-footer">
         <p class="copyright"><small>©2019 Takanashi Design</small></p>
+        <p id="page-top"><a href="#wrap">PAGE TOP</a></p>
     </footer>
     <?php wp_footer(); ?> 
+    <script>
+    /* top-pageに戻る処理 */
+    $(function() {
+        var topBtn = $('#page-top');
+        topBtn.hide();
+    
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                topBtn.fadeIn();
+            } else {
+                topBtn.fadeOut();
+            }
+        });
+        /* スクロールしてトップ */
+        topBtn.click(function () {
+            $('body,html').animate({
+            scrollTop: 0
+            }, 300);
+            return false;
+        });
+    });
+    </script>
 </html>
