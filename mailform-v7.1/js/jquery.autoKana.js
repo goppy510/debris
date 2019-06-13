e3dd7c586ed1518340f75fc6c44ab84d02192713
goppy510 <tacumin.goppy.06@vesta.ocn.ne.jp>
2019-06-14 08:46:14 +0900

    wpのjquery使わないようにした

--- mailform-v7.1/js/jquery.autoKana.js
+++ mailform-v7.1/js/jquery.autoKana.js
@@ -22,7 +22,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-(function ($) {
+$(function() {
     $.fn.autoKana = function (element1, element2, passedOptions) {
 
         var options = $.extend(
@@ -195,4 +195,4 @@
             }
         }
     };
-})(jQuery);
+});
