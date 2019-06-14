5f432c7f8ac4fcab7d2a65b805629962ac38d8d4
goppy510 <tacumin.goppy.06@vesta.ocn.ne.jp>
2019-06-14 09:26:53 +0900

    ファイル名変更

--- mailform-v7.1/php/config.php
+++ mailform-v7.1/php/config.php
@@ -13,7 +13,7 @@ $rm_send_address[] = 'yabamy.001@gmail.com';
 
 
 //【必須】 サンクスページのURL -- index.htmlからの相対パス、またはhttp://からの絶対パス --
-$rm_thanks_page_url = 'thanks.html';
+$rm_thanks_page_url = '../thanks-mail.php';
 
 
 
