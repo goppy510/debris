3436d48369bd2333558d6991047c608f922c2b26
goppy510 <tacumin.goppy.06@vesta.ocn.ne.jp>
2019-06-14 09:26:18 +0900

    サンクスメール画面ãを非表示設定

--- header-index.php
+++ header-index.php
@@ -15,9 +15,11 @@
                 // グローバルメニューを表示する(固定ページを管理画面から作ってあること必須)
                 $main_menu = wp_get_nav_menu_items('menu', array());
                 foreach ($main_menu as $menu) {
-                    $icon = wp_get_attachment_image_src($menu->icon, 'full');
-                    $alt = get_post_meta($menu->icon, '_wp_attachment_image_alt', true);
-                    echo '<li><a href="'.$menu->url.'"><img src="'.$icon[0].'" alt="'.$alt.'">'.$menu->title.'</a></li>';
+                    if ($menu->title != 'thanks-mail') {
+                        $icon = wp_get_attachment_image_src($menu->icon, 'full');
+                        $alt = get_post_meta($menu->icon, '_wp_attachment_image_alt', true);
+                        echo '<li><a href="'.$menu->url.'"><img src="'.$icon[0].'" alt="'.$alt.'">'.$menu->title.'</a></li>';
+                    }
                 }
                 ?>
             </ul>
