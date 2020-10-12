5fca6405fd1f54b7221b8a7fa8865ef4776344e4
tgoto <tacumin.goppy.06@vesta.ocn.ne.jp>
2020-10-12 10:51:04 +0900

    add javascript for animation

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,18 @@
 <div id="side-nav">
-  <?php wp_nav_menu(array('theme_location' => 'sidebar_nav')); ?>
+    <?php wp_nav_menu(array('theme_location' => 'sidebar_nav')); ?>
 </div><!-- end of #side-nav -->
+
+<script>
+    $(document).ready(function() {
+        $('#side-nav .menu li a').mouseenter(
+            function() {
+                $(this).stop(false, false).animate({right: 10}, 300);
+            }
+        );
+        $('#side-nav .menu li a').mouseleave(
+            function() {
+                $(this).stop(false, true).animate({right: 0}, 300);
+            }
+        );
+    });
+</script>
