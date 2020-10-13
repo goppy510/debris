1745f7ac0e120b31340a4be5a6eec207b0d3529f
tgoto <tacumin.goppy.06@vesta.ocn.ne.jp>
2020-10-13 18:41:09 +0900

    add vertical solid

--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,21 @@
-<div id="side-nav">
-    <?php wp_nav_menu(array('theme_location' => 'sidebar_nav')); ?>
-</div><!-- end of #side-nav -->
+<div id="side-area">
+    <div id="side-line">
+        <div id="side-nav">
+            <?php wp_nav_menu(array('theme_location' => 'sidebar_nav')); ?>
+        </div><!-- end of #side-nav -->
+    </div>
+</div>
 
 <script>
     $(document).ready(function() {
         $('#side-nav .menu li a').mouseenter(
             function() {
-                $(this).stop(false, false).animate({right: 10}, 300);
+                $(this).stop(true, true).animate({right: 20}, 300);
             }
         );
         $('#side-nav .menu li a').mouseleave(
             function() {
-                $(this).stop(false, true).animate({right: 0}, 300);
+                $(this).stop(true, true).animate({right: 0}, 300);
             }
         );
     });
