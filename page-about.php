c5727ccf81e1865cc2d2a6eb7c1c1dbe1627415a
tgoto <tacumin.goppy.06@vesta.ocn.ne.jp>
2021-01-17 11:43:23 +0900

    fix: aboutの文字を廃止し画像で入れた

--- page-about.php
+++ page-about.php
@@ -3,20 +3,32 @@
 Template Name:About
 */
 ?>
-
-<?php get_header('index'); ?>
-<div class="main-area">
-    <div class="about">
-        <ul>
-            <li><span class="about-head">About_</span></li>
-            <br/>
-            <li><span class="designer-head">Designer</span></li>
-            <br/>
-            <li><span class="designer-name-jp">高梨 仁史</span><span class="designer-name-en"> Hitoshi Takanashi</span></li>
-            <br/>
-            <li><span class="designer-name-jp">岡野 雅美</span><span class="designer-name-en"> Miyabi Okano</span></li>
-        </ul>
-    </div><!-- about -->
-</div ><!-- main-area -->
+<?php get_header(); ?>
+<?php
+    $path = get_template_directory_uri().'/img/about/';
+    $img = 'designer.png';
+?>
+<body <?php body_class(); ?>>
+    <?php get_sidebar(); ?>
+    <div class="page-header">
+        <div class="links">
+            <ul>
+                <?php
+                // グローバルメニューを表示する(固定ページを管理画面から作ってあること必須)
+                $main_menu = wp_get_nav_menu_items('menu', array());
+                foreach ($main_menu as $menu) {
+                    if ($menu->title != 'thanks-mail') {
+                        $icon = wp_get_attachment_image_src($menu->icon, 'full');
+                        $alt = get_post_meta($menu->icon, '_wp_attachment_image_alt', true);
+                        echo '<li><a href="'.$menu->url.'"><img src="'.$icon[0].'" alt="'.$alt.'">'.$menu->title.'</a></li>';
+                    }
+                }
+                ?>
+            </ul>
+        </div><!-- links -->
+        <div class="about">
+            <?php image64($img, $path) ?>
+        </div><!-- about -->
+    </div><!-- page-header -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
